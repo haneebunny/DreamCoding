44b21db27f95f76c55d4b0b316ce685a1c377239
haneebunny <re____@naver.com>
2022-12-23 14:11:54 +0900

    :art: profile

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -1,11 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Profile from "./components/Profile";
+import Avatar from "./components/Avatar";
 
 function AppProfile() {
   return (
     <>
-      <Profile image="/img/Hi.jpg" name="hi" desc="프론트엔드 개발자" />
+      <Profile
+        isNew={true}
+        image="/img/Hi.jpg"
+        name="hi"
+        desc="프론트엔드 개발자"
+      />
+      <Profile
+        // isNew={false} 이건 없어도 무방함
+        image="/img/Hi.jpg"
+        name="so cute"
+        desc="백엔드 개발자"
+      />
+      <Profile image="/img/Hi.jpg" name="happy" desc="난 개발자" />
+      <Avatar image="/img/Hi.jpg" name="avatar" isNew={true} />
     </>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Avatar from "./Avatar";
 
-export default function Profile({ image, name, desc }) {
+export default function Profile({ image, name, desc, isNew }) {
   return (
     <div className="profile">
-      <img className="photo" src={image} alt="avatar" />
+      {/* <img className="photo" src={image} alt="avatar" />
+      {isNew && <span className="new">NEW</span>} */}
+      <Avatar image={image} isNew={isNew} />
       <h1>{name}</h1>
       <p>{desc}</p>
     </div>
