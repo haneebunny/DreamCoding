501a8b9fbac3d0592627bf8f122aa90a8d623513
haneebunny <re____@naver.com>
2023-01-06 16:57:11 +0900

    :sparkles: class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppMentors from "./AppMentors";
+import AppProducts from "./basic/AppProducts";
 import "./index.css";
 
 import reportWebVitals from "./reportWebVitals";
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppMentors />
+    <AppProducts />
   </React.StrictMode>
 );
 
